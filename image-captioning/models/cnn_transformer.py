334090e078cca660bd7498344c3baf9fe569e146
GabrielDornelles <kasugayui2@gmail.com>
2023-01-06 19:38:49 -0300

    makes more sense, still not working

--- image-captioning/models/cnn_transformer.py
+++ image-captioning/models/cnn_transformer.py
@@ -7,14 +7,30 @@ from dataset import Flickr
 import torchvision.transforms as transforms
 
 
+# class Encoder(nn.Module):
+
+#     def __init__(self, embed_size, freeze_weights : bool = True) -> None:
+#         super().__init__()
+#         self.convnet.classifier[1] = nn.Linear(in_features=1280, out_features=embed_size, bias=True)
+#         self.dropout = nn.Dropout(0.5)
+
+#         if freeze_weights:
+#             for name, parameter in self.convnet.named_parameters():
+#                 if "classifier" in name:
+#                     parameter.requires_grad = True
+#                 else:
+#                     parameter.requires_grad = False
+
+#     def forward(self, x):
+#         features = self.convnet(x) # torch.Size([1, embed_size])
+#         return self.dropout(F.relu(features))
+
 class Encoder(nn.Module):
 
-    def __init__(self, embed_size, freeze_weights : bool = True) -> None:
+    def __init__(self, freeze_weights : bool = True) -> None:
         super().__init__()
         self.convnet = torchvision.models.efficientnet_b0(weights=torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1)
-        self.convnet.classifier[1] = nn.Linear(in_features=1280, out_features=embed_size, bias=True)
-        self.dropout = nn.Dropout(0.5)
-
+       
         if freeze_weights:
             for name, parameter in self.convnet.named_parameters():
                 if "classifier" in name:
@@ -23,14 +39,20 @@ class Encoder(nn.Module):
                     parameter.requires_grad = False
 
     def forward(self, x):
-        features = self.convnet(x) # torch.Size([1, embed_size])
-        return self.dropout(F.relu(features))
+        x = self.convnet.features[0](x) 
+        x = self.convnet.features[1](x)
+        x = self.convnet.features[2](x)
+        x = self.convnet.features[3](x)
+        x = self.convnet.features[4](x)
+        x = self.convnet.features[5](x)
+        x = self.convnet.features[6](x)
+        x = self.convnet.features[7](x)
+        return x
 
 
 class ImageCaptioningModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, num_layers, num_heads, dropout):
         super().__init__()
-        
         
         self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
@@ -40,41 +62,62 @@ class ImageCaptioningModel(nn.Module):
         self.dropout = dropout
         
         # Image feature encoder
-        self.cnn_encoder = Encoder(embedding_dim)
+        self.cnn_encoder = Encoder()
 
         # Embedding layer to convert word indices to word embeddings
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         
         # Transformer encoder to process image features
         self.encoder = nn.TransformerEncoder(
-            nn.TransformerEncoderLayer(hidden_dim, num_heads, 1024 * 1, dropout),
+            nn.TransformerEncoderLayer(
+                hidden_dim,
+                num_heads, 
+                1024 * 1,
+                dropout
+            ),
             num_layers
         )
         
         # Transformer decoder to generate captions
         self.decoder = nn.TransformerDecoder(
-            nn.TransformerDecoderLayer(hidden_dim, num_heads, 1024 * 1, dropout), 
+            nn.TransformerDecoderLayer(
+                hidden_dim, 
+                num_heads, # number of self attentions before concat and linear
+                1024 * 1, 
+                dropout), 
             num_layers
-        )
+        ) # encodes the information about how each feature relates to each feature
         
         # Linear layer to map output of decoder to word embeddings
         self.output_linear = nn.Linear(hidden_dim, vocab_size)
         
     def forward(self, image, captions):
         image_features = self.cnn_encoder(image)
-
+        image_features = image_features.reshape(
+            image_features.shape[-1] * image_features.shape[-2], 
+            image_features.shape[0], 
+            image_features.shape[1]
+        ) # [B,C,H,W] => [H * W, B , C], now image features is a sequence of tokens of length H * W
+        # I really dont like not using batch_first, but captions are not batch_first here
+      
         # Convert word indices to word embeddings
+        # TODO: mask the captions with look ahead mask
         embedded_captions = self.embedding(captions)
-        embedded_captions = torch.cat((image_features.unsqueeze(0), embedded_captions), dim=0)
+        
 
-        # Pass image features through encoder
-        thought_vectors = self.encoder(embedded_captions)
+        thought_vectors = image_features
 
         # Pass thought vectors and embedded captions through decoder
-        decoder_output = self.decoder(thought_vectors, embedded_captions)
-        
+        print(f"{thought_vectors.shape=}")
+        print(f"{embedded_captions.shape=}")
+        decoder_output = self.decoder(tgt=thought_vectors, memory=embedded_captions)
+        print(f"{decoder_output.shape=}")
         # Map decoder output to word embeddings
         logits = self.output_linear(decoder_output)
+        print(f"{logits.shape=}")
+        # TODO: logits shape currently are torch.Size([100, 8, 2994]).
+        # Understand how to interpret this 100 tokens (this is the final caption)
+        # probably just pad caption to be fixed at 100 tokens?
         return logits
     
     def sample(self, image, vocabulary, max_length=20, device=torch.device("cuda")):
